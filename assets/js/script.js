8c62bcb7958f8b40c83abd922addb062401839a3
Darren <darrenrobinsonpt@gmail.com>
2024-03-24 21:49:03 +0000

    code added to find two random numbers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener(`click`, function() {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`) 
+            }
+        })
+    }
+})
+
+/** This main game "loop" is called when the script is first loaded.
+ * And after the user's answer has been processed.
+ */
 function runGame () {
 
+    // Creates two random numbers
+    let num1 = Math.floor(math.random() * 25) + 1;
+    let num2 = Math.floor(math.random() * 25) + 1;
 }
 
 function checkAnswer () {
